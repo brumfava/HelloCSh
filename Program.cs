9047253a41d9d1777d344d159b2419284de75c09
Bruno Fava <106696478+brumfava@users.noreply.github.com>
2024-12-10 23:28:36 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        string cFrase = "";
+
+        Console.WriteLine("Primeiro nome:");
+        string cFirstNome = Console.ReadLine();
+
+        Console.WriteLine("Sobrenome(s):");
+        string cLastNome = Console.ReadLine();
+
+        if (!string.IsNullOrEmpty(cFirstNome) || !string.IsNullOrEmpty(cLastNome))
+        {
+            if (!string.IsNullOrEmpty(cFirstNome) && string.IsNullOrEmpty(cLastNome)) 
+            {
+                cFrase = "Seu nome é " + cFirstNome;    
+            }
+
+            else if (string.IsNullOrEmpty(cLastNome) && !string.IsNullOrEmpty(cLastNome)) 
+            {
+                cFrase = "Seu sobrenome é " + cLastNome;
+               
+            }
+
+            else if (!string.IsNullOrEmpty(cLastNome) && !string.IsNullOrEmpty(cLastNome)) 
+            {
+                cFrase = "Seu nome completo é:" +$"{cFirstNome} {cLastNome}"; 
+            }  
+
+            Console.WriteLine(cFrase);      
+        }
+    }
+}
